9dc7c8bedaf45949e7e7d649ded858fc883de87b
Pedro Manuel da Silva Alves <pedrosilvaalves@hotmail.com>
2018-11-22 17:43:02 +0000

    Iniciar Projeto

---
+++ PWEB_Estagios/PWEB_Estagios/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PWEB_Estagios.Startup))]
+namespace PWEB_Estagios
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
