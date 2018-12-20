d4f2f979fbe2b9e16c65510842dd9f6e2e68deca
Pedro Manuel da Silva Alves <pedrosilvaalves@hotmail.com>
2018-12-20 19:33:22 +0000

    CreateProposta Get
    Falta o httppost

--- PWEB_Estagios/PWEB_Estagios/Controllers/AlunoController.cs
+++ PWEB_Estagios/PWEB_Estagios/Controllers/AlunoController.cs
@@ -2,6 +2,7 @@
 using PWEB_Estagios.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -29,6 +30,41 @@ namespace PWEB_Estagios.Controllers
         {
             string strCurrentUserId = User.Identity.GetUserId();
             return View(context.Alunos.Where(s => s.UserId == strCurrentUserId).FirstOrDefault());
+        }
+        [HttpPost]
+        [Authorize(Roles = "Aluno")]
+        public ActionResult Edit(Aluno aluno)
+        {
+            if(aluno != null)
+            {
+
+                string strCurrentUserId = User.Identity.GetUserId();
+                Aluno contaAluno = context.Alunos.Where(s => s.UserId == strCurrentUserId).FirstOrDefault();
+                contaAluno.PrimeiroNome = aluno.PrimeiroNome;
+                contaAluno.Apelido = aluno.Apelido;
+                contaAluno.NumeroAluno = aluno.NumeroAluno;
+                contaAluno.NumeroCadeirasConcluidas = aluno.NumeroCadeirasConcluidas;
+                contaAluno.Media = aluno.Media;
+            if (ModelState.IsValid)
+            {
+                    var contaToUpdate = context.Alunos.Where(s => s.UserId == strCurrentUserId).FirstOrDefault();
+                    if(TryUpdateModel(contaToUpdate,"",new string[] {"PrimeiroNome", "Apelido", "NumeroAluno","NumeroCadeirasConcluidas", "Media" }))
+                    {
+                        try
+                        {
+                            context.SaveChanges();
+                            return RedirectToAction("Perfil");
+                        }
+                        catch (Exception)
+                        {
+                            ModelState.AddModelError("", "Não foi possivel atualizar o modelo!");
+                        }
+                    }
+                return RedirectToAction("Perfil");
+            }
+
+            }
+            return View(aluno);
         }
 
     }
---
+++ PWEB_Estagios/PWEB_Estagios/Controllers/PropostaController.cs
@@ -0,0 +1,43 @@
+﻿using PWEB_Estagios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PWEB_Estagios.Controllers
+{
+    public class PropostaController : Controller
+    {
+        private PropostasContext context = new PropostasContext();
+        // GET: Proposta
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Docente")]
+        public ActionResult Create()
+        {
+            IList<String> nomeDocentes = new List<String>();
+            IList<String> nomeEmpresas = new List<String>();
+            foreach (var i in context.Docentes)
+            {
+                nomeDocentes.Add(i.PrimeiroNome+" " + i.Apelido + " : " + i.NumeroDocente);
+            }
+            foreach (var i in context.Empresas)
+            {
+                nomeEmpresas.Add(i.Nome + " " + i.Sede + " : " + i.EmpresaNIF);
+            }
+            ViewBag.Docentes = new SelectList(nomeDocentes.ToList());
+            ViewBag.Empresas = new SelectList(nomeEmpresas.ToList());
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Proposta proposta)
+        {
+
+            return View(proposta);
+        }
+    }
+}
